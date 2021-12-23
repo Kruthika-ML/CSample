c2190a0558d7357a47f8d144fd62020dbcbc8426
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-23 11:51:45 +0530

    Add files via upload

---
+++ Samp.java
@@ -0,0 +1,9 @@
+public class Samp {
+    public static void main(String[] args) {
+        byte b = 10, c = 20;
+        String name = "Wayne";
+
+        System.out.println("The sum of numbers is " +(b+c));
+        System.out.println("Hello " +name);
+    }
+}
