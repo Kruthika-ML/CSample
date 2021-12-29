ecd8a78272379355fa5d5388e1f34c180587e14a
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-29 11:14:17 +0530

    Add files via upload

---
+++ Demo5.java
@@ -0,0 +1,17 @@
+package General;
+
+public class Demo5 {
+    public static void main(String[] args) {
+
+        int x = 10;
+        int y = 11;
+
+        if (!(x<y)||(x==y)) {                               //x>y && x!=y
+            System.out.println("COndition is true");
+        }
+        else
+        {
+            System.out.println("Condition is false");
+        }
+    }
+}
