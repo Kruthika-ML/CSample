e56238902671c9be0a4bc72eae46ee765420da3e
Kruthika R <65350577+Kruthika-ML@users.noreply.github.com>
2021-12-29 11:00:47 +0530

    Add files via upload

---
+++ Demo3.java
@@ -0,0 +1,14 @@
+package General;
+
+public class Demo3 {
+    public static void main(String[] args) {
+        int x = 5;
+
+        if(x!=5){
+            System.out.println("Value of x is not 5");
+        }
+        else {
+            System.out.println("Value of x is 5");
+        }
+    }
+}
